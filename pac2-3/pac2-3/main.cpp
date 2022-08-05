103267b21d5d25a6a1f4c9542bf71488b5d5a308
maruyamakouhei <107020852+marukoisan@users.noreply.github.com>
2022-08-05 15:29:05 +0900

    dxlibを適用しました

---
+++ pac2-3/pac2-3/main.cpp
@@ -0,0 +1,33 @@
+#include"DxLib.h"
+//変数の宣言
+int g_LoopCount = 0;
+//プログラムの開始
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine, int nCmdShow)
+{
+	//ウィンドウモードで起動する
+	ChangeWindowMode(TRUE);
+ 
+	//DXライブラリの初期化処理:エラーが起きたら直ちに終了
+	if (DxLib_Init() == -1)return-1;
+ 
+	//描画先画面を裏にする
+	SetDrawScreen(DX_SCREEN_BACK);
+ 
+	//ゲームループ
+	while (ProcessMessage() == 0)
+	{
+		//画面の初期化
+		ClearDrawScreen();
+ 
+		//ループカウンタの表示
+		DrawFormatString(0, 0, GetColor(255, 255, 255), "LoopCount=%d", g_LoopCount++);
+ 
+		//裏画面の内容を表画面に反映
+		ScreenFlip();
+	}
+	//DXライブラリ使用の終了処理
+	DxLib_End();
+	//ソフトの終了
+	return 0;
+
+}
