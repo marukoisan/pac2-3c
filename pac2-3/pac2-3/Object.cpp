057d7894f898e3ba37d1d4b0a7ac85b6fa618d19
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2022-08-15 13:16:52 +0900

    当たり判定objectファイルの追加

---
+++ pac2-3/pac2-3/Object.cpp
@@ -0,0 +1,84 @@
+#include "Object.h"
+
+//-------------------------
+// 絶対値を求める
+//-------------------------
+float absf(void* num) 
+{
+	if(*(float*)num<0)
+	{
+		*(float*)num = *(float*)num * -1;
+	}
+	return *(float*)num;
+}
+
+//--------------------------
+// 矩形と矩形の当たり判定 
+//--------------------------
+bool CheckHitBox(CObject* obj1, CObject* obj2)
+{
+	//old, nowの更新
+	obj1->UpdateFlg();
+	obj2->UpdateFlg();
+
+	//必要な情報の準備
+	float distanceX = obj1->GetX() - obj2->GetX();
+	float distanceY = obj1->GetY() - obj2->GetY();
+	float rangeX = obj1->GetWidth() / 2 + obj2->GetWidth() / 2;
+	float rangeY = obj1->GetHeight() / 2 + obj2->GetHeight() / 2;
+
+	//-------------------------------
+	// 	当たった瞬間
+	//-------------------------------
+	if (obj1->GetOldHitFlg() == false 
+		&& obj2->GetOldHitFlg() == false
+		&&absf(&distanceX) <= rangeX 
+		&& absf(&distanceY) <= rangeY) 
+	{
+		obj1->SetNowFlg(true);
+		obj2->SetNowFlg(true);
+
+		obj1->HitAction();
+		obj2->HitAction();
+
+		return true;
+	}
+	//------------------------------
+	//	重なっている間
+	//------------------------------
+	else if(obj1->GetOldHitFlg() == true 
+		&& obj2->GetOldHitFlg() == true
+		&& absf(&distanceX) <= rangeX
+		&& absf(&distanceY) <= rangeY)
+	{
+		obj1->OverLapAction();
+		obj2->OverLapAction();
+	}
+	else //それ以外は当たっていない(false)
+	{
+		obj1->SetNowFlg(false);
+		obj2->SetNowFlg(false);
+	}
+
+
+	//-----------------
+	//  離れたとき
+	//-----------------
+	if (obj1->GetOldHitFlg() == true
+		&& obj2->GetOldHitFlg() == true
+		&& obj1->GetNowHitFlg() == false
+		&& obj2->GetNowHitFlg() == false)
+	{
+		obj1->ReleaseAction();
+		obj2->ReleaseAction();
+	}
+
+	return false;
+}
+
+//--------------------------------
+// 円と矩形の当たり判定
+//--------------------------------
+bool CheckHitBox_Circle(CObject* box, CObject* circle) {
+
+}
---
+++ pac2-3/pac2-3/Object.h
@@ -0,0 +1,41 @@
+#pragma once
+class CObject
+{
+protected:
+	//ピボット位置
+	float x = 0;
+	float y = 0;
+
+	//当たり判定
+	float height = 0;
+	float width = 0;
+
+	//判定用変数
+	bool oldHitFlg = false;
+	bool nowHitFlg = false;
+
+public:
+	virtual void Update() = 0;
+	virtual void Draw()const = 0;
+	void UpdateFlg() { oldHitFlg = nowHitFlg; }
+
+	//当たった時の処理
+	virtual void HitAction() = 0;
+	virtual void OverLapAction() {}
+	virtual void ReleaseAction() = 0;
+
+	//get関数
+	float GetX()const { return x; }
+	float GetY()const { return y; }
+	float GetHeight()const { return height; }
+	float GetWidth()const { return width; }
+	bool GetOldHitFlg()const { return oldHitFlg; }
+	bool GetNowHitFlg()const { return nowHitFlg; }
+
+	//set関数
+	void SetNowFlg(bool flg) { nowHitFlg = flg; }
+};
+
+//矩形と矩形の当たり判定
+bool CheckHitBox(CObject* obj1, CObject* obj2);
+bool CheckHitBox_Circle(CObject* box,CObject* circle);
