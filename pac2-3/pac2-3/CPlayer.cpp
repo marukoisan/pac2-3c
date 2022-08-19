b08de3aa3e0fae818b7518a6e913d0d369b423ec
tamayosekento <107020913+tamayosekento@users.noreply.github.com>
2022-08-19 11:54:06 +0900

    当たった時の処理
    敵に当たるとプレイヤーが消滅する(途中)

--- pac2-3/pac2-3/CPlayer.cpp
+++ pac2-3/pac2-3/CPlayer.cpp
@@ -2,7 +2,37 @@
 #include"DxLib.h"
 #include"CPlayer.h"
 
+int i;
+int pacmanDying[11];
+
+int LoadImage()
+{
+	if (LoadDivGraph("images/sprites/dying.png", 11, 11, 1, 20, 20, pacmanDying) == -1)return 1;
+}
+
 void HitAction()
 {
+	for (i = 0; i < 11; i++) {
+        //描画
+        DrawGraph(0, 0, pacmanDying[i], FALSE);
 
+        // アニメーションパターンナンバーを変更
+        i++;
+        if (i == 10) i = 0;
+
+        //一定時間待つ
+        WaitTimer(100);
+	}
+
+    // 読み込んだ画像を削除
+    for (i = 0; i < 11; i++)
+    {
+        DeleteGraph(pacmanDying[i]);
+    }
+
+}
+
+int CPlayer::LoadImage()
+{
+	return 0;
 }
--- pac2-3/pac2-3/CPlayer.h
+++ pac2-3/pac2-3/CPlayer.h
@@ -1,9 +1,11 @@
 #pragma once
 #include"Object.h"
 
-class CPlayer : public CObject {
-private:
-
+class CPlayer : public CObject 
+{
+protected:
+	//•ªŠ„“Ç‚İ‚İ
+	int pacmanDying[11];
 public:
-	
+	int LoadImage();
 };
