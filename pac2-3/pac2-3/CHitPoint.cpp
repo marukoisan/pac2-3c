f490c895962a562d405afc49a78b2369508ac7d9
tamayosekento <107020913+tamayosekento@users.noreply.github.com>
2022-08-22 15:01:33 +0900

    残機
    プレイヤーの残機の表示(途中)

---
+++ pac2-3/pac2-3/CHitPoint.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"DxLib.h"
+#include"CHitPoint.h"
+
+CHitPoint::CHitPoint()
+{
+	LoadDivGraph("images/sprites/dying.png",)
+}
---
+++ pac2-3/pac2-3/CHitPoint.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class CHitPoint
+{
+private:
+	CHitPoint();
+	~CHitPoint() {}
+};
