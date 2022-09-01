af93e304ebfff57defff4d0ab1e84665fd3118df
maruyamakouhei <107020852+marukoisan@users.noreply.github.com>
2022-09-02 03:54:25 +0900

    クラスを二つに分けて何がしたいかは明確に出来たかもです

--- pac2-3/pac2-3/Player2.h
+++ pac2-3/pac2-3/Player2.h
@@ -1,24 +1,29 @@
 #pragma once
 #include"Object.h"
 #define PI	3.1415926535897932384626433832795f
+#define PACMAN_IMAGE 12
+#define PACMAN_ORIGINAL_SIZE 32.0
 #define PACMAN_NEW_SIZE  20.0
 
 
 class Player2 : public CObject
 {
 private:
-	int gPacman[12];
-	bool wallSize;
+	int gPacman[PACMAN_IMAGE];
+	bool wallHit;
 
 public:
 	Player2();
 	~Player2();
 	void Update() {}
 	void Draw()const;
-	void HitAction() { wallSize = false; }//0が入っている
+	void HitAction() { wallHit = false; }//0が入っている
 	int LoadImages();
-	int Init();
-	bool GetFlg() { return wallSize; }
+	int Init(float x,float y);
+	bool GetFlg() { return wallHit; }
+	/*
+	パックマンの縦横の奴をこっちで書くかコントローラー側で書くか
+	*/
 
 };
 
