2293aed5765536e53d3ac5ace62e965f7f6c2272
biseryuki <107020902+biseryuki@users.noreply.github.com>
2022-09-20 23:57:52 +0900

    タイトル途中

--- pac2-3/pac2-3/CTitle.cpp
+++ pac2-3/pac2-3/CTitle.cpp
@@ -2,6 +2,7 @@
 #include"DxLib.h"
 #include "CTitle.h"
 #include"CGameMain.h"
+#define TILESIZE 20
 
 //----------------------------
 // コンストラクタ
@@ -29,6 +30,41 @@ CAbstractScene* CTitle::Update()
 	{
 		return new CGameMain;
 	}
+
+
+	timer++;
+
+	//パックマンスピード
+	if (timer / 15 >= 18 && D_TITLE_POS_X + 5 * TILESIZE <= D_TITLE_POS_X + 28 * TILESIZE + playerx)
+	{
+	playerx-=3;
+	}
+
+	angle = -M_PI / 2;	//左向き
+
+
+	//画像読み込み
+	nickname = LoadGraph("images/nickname.png");
+	akabei_font = LoadGraph("images/akabei.png");
+	pinky_font = LoadGraph("images/PINKY.png");
+	aosuke_font = LoadGraph("images/AOSUKE.png");
+	guzuta_font = LoadGraph("images/guzuta.png");
+	highscore = LoadGraph("images/highscore.png");
+	slash = LoadGraph("images/slash.png");
+	nickname = LoadGraph("images/nickname.png");
+	oikake = LoadGraph("images/oikake.png");
+	machibuse = LoadGraph("images/machibuse.png");
+	kimagure = LoadGraph("images/kimagure.png");
+	otoboke = LoadGraph("images/otoboke.png");
+	akabei_title = LoadGraph("images/akabei_title.png");
+	pinky_title = LoadGraph("images/pinky_title.png");
+	aosuke_title = LoadGraph("images/aosuke_title.png");
+	guzuta_title = LoadGraph("images/guzuta_title.png");
+	esa = LoadGraph("images/tiles/big_dot.png");
+
+	//パックマン
+	LoadDivGraph("images/sprites/pacman.png", D_PLAYER_IMAGE_MAX, 3, 1, 32, 32, pacman);
+
 	return this;
 }
 
@@ -37,6 +73,101 @@ CAbstractScene* CTitle::Update()
 //---------------------
 void CTitle::Draw()const
 {
-	DrawGraph(D_TITLE_POS_X + 140, 100, charactar_strImage, TRUE);
+	DrawGraph(D_TITLE_POS_X +  9 * TILESIZE,   0, highscore, TRUE);
+
+	DrawGraph(D_TITLE_POS_X +  7 * TILESIZE, 100, charactar_strImage, TRUE);
+	DrawGraph(D_TITLE_POS_X + 18 * TILESIZE, 100, slash, TRUE);
+	DrawGraph(D_TITLE_POS_X + 21 * TILESIZE, 100, nickname, TRUE);
+
+
+	//アカベイ
+	if (timer / 15 >= 1)
+	{
+		DrawRotaGraphF(D_TITLE_POS_X + 5 * TILESIZE, 145, 1.8, 0, akabei_title, TRUE);
+	}
+	if (timer / 15 >= 3)
+	{
+		DrawGraph(D_TITLE_POS_X +  8 * TILESIZE, 140, oikake, TRUE);
+	}
+	if (timer / 15 >= 4)
+	{
+		DrawGraph(D_TITLE_POS_X + 18 * TILESIZE, 140, akabei_font, TRUE);
+
+	}
+
+
+	//ピンキー
+	if (timer / 15 >= 5)
+	{
+		DrawRotaGraphF(D_TITLE_POS_X + 5 * TILESIZE, 185, 1.8, 0, pinky_title, TRUE);
+	}
+	if (timer / 15 >= 7)
+	{
+		DrawGraph(D_TITLE_POS_X +  8 * TILESIZE, 180, machibuse, TRUE);
+	}
+	if (timer / 15 >= 8)
+	{
+		DrawGraph(D_TITLE_POS_X + 19 * TILESIZE, 180, pinky_font, TRUE);
+	}
+
+
+	//アオスケ
+	if (timer / 15 >= 9)
+	{
+		DrawRotaGraphF(D_TITLE_POS_X + 5 * TILESIZE, 225, 1.8, 0, aosuke_title, TRUE);
+	}
+	if (timer / 15 >= 11)
+	{
+		DrawGraph(D_TITLE_POS_X +  8 * TILESIZE, 220, kimagure, TRUE);
+	}
+	if (timer / 15 >= 12)
+	{
+		DrawGraph(D_TITLE_POS_X + 18 * TILESIZE, 220, aosuke_font, TRUE);
+	}
+
+
+	//グズタ
+	if (timer / 15 >= 13)
+	{
+		DrawRotaGraphF(D_TITLE_POS_X + 5 * TILESIZE, 265, 1.8, 0, guzuta_title, TRUE);
+	}
+	if (timer / 15 >= 15)
+	{
+		DrawGraph(D_TITLE_POS_X +  8 * TILESIZE, 260, otoboke, TRUE);
+	}
+	if (timer / 15 >= 16)
+	{
+		DrawGraph(D_TITLE_POS_X + 18 * TILESIZE, 260, guzuta_font, TRUE);
+	}
+
+
+	//エサ
+	if (timer / 15 >= 17 && timer / 15 <= 18)
+	{
+		DrawRotaGraphF(D_TITLE_POS_X + 5 * TILESIZE, 320, 0.2, 0, esa, TRUE);
+	}
+	if (timer / 15 >= 17)
+	{
+		if (timer / 15 % 2)
+		{
+			DrawRotaGraphF(D_TITLE_POS_X + 5 * TILESIZE, 320, 0.2, 0, esa, TRUE);
+		}
+	}
+
+
+	//パックマン
+	if (timer / 15 >= 18)
+
+	{
+		if (timer / D_TITLE_POS_X + 5 * TILESIZE <= D_TITLE_POS_X + 28 * TILESIZE + playerx)
+		{
+			DrawRotaGraphF(D_TITLE_POS_X + 28 * TILESIZE + playerx, 320, 1.0, angle, pacman[timer / D_PLAYER_ANIM_FPS % D_PLAYER_IMAGE_MAX], TRUE);
+		}
+	}
+
+	
+
 	DrawFormatString(0, 0, 0xffffff, "%d", saveData);
+
+	
 }
--- pac2-3/pac2-3/CTitle.h
+++ pac2-3/pac2-3/CTitle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "CAbstractScene.h"
+#include"CPlayer.h"
 
 #define D_TITLE_POS_X (360)
 
@@ -8,6 +9,37 @@ class CTitle : public CAbstractScene
 private:
 
 	int charactar_strImage;
+
+	//画像用変数
+	int akabei_font;
+	int pinky_font;
+	int aosuke_font;
+	int guzuta_font;
+	int highscore;
+	int slash;
+	int nickname;
+	int oikake;
+	int machibuse;
+	int kimagure;
+	int otoboke;
+	int akabei_title;
+	int pinky_title;
+	int aosuke_title;
+	int guzuta_title;
+	int esa;
+
+	//タイマー
+	int timer;
+
+	//向き
+	float angle;
+
+	//パックマンアニメーション
+	int pacman[D_PLAYER_IMAGE_MAX] = {};
+
+	//パックマンスピード
+	float playerx;
+
 
 public:
 	CTitle();
